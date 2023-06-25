fc7f3601ba09a33f693d6cf4e17f3f7759bf56d5
Mugabe Ignace <ignacemugabe@gmail.com>
2023-06-25 19:58:05 +0200

    create Library UI

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 let myLibrary = [];
 
-function Book ()
+class Book
 {
-    // the constructor...
+    constructor(name, author, pages, status)
+    {
+        this.name = name
+        this.author = author
+        this.pages = pages
+        this.status = status
+    }
+    deleteBook ()
+    {
+        console.warn("Book deleted!");
+    }
 }
+
+
+class Novel extends Book
+{
+    constructor(name, author, pages, status)
+    {
+        super(name, author, pages, status)
+    }
+}
+
+const newNovelBook = new Novel("Hope", "Mr Bean", "128");
+
 
 function addBookToLibrary ()
 {
     // do stuff here
 }
+
